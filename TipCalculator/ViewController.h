c021ed23544d096e8decb922a52dee8b88022874
jpila <j.pilapil@hotmail.com>
2016-07-08 12:28:28 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by JOSE PILAPIL on 2016-07-08.
+//  Copyright © 2016 JOSE PILAPIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
