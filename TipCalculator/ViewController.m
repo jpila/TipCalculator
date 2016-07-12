dc707c166767a8d60b2030e58e4f47fb244e6544
jpila <j.pilapil@hotmail.com>
2016-07-12 15:38:54 -0400

    tipcalculator basic

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -16,12 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.billAmountTextField.delegate = self;
+    self.billAmountTextField.text = @"Input Bill";
+    // label is for displaying the output
+    self.tipAmountLabel.text = @"";
+    // numberOfLines = 0 makes the label multiline display
+    self.tipAmountLabel.numberOfLines = 0;
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)textViewDidChange:(UITextView *)textView {
+    self.tipAmountLabel.text = self.billAmountTextField.text;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)calculateTip:(id)sender {
+    double price = self.billAmountTextField.text.doubleValue * .15;
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"%f",price];[self.view setNeedsLayout];
+    
 }
 
 @end
